ef2ea041672314f0c7afd6e5f137f60c68bb54e0
SWH <sungwoo166@gmail.com>
2021-12-16 17:34:39 +0900

    마이페이지 url 지정
    .

--- store/urls.py
+++ store/urls.py
@@ -12,4 +12,19 @@ urlpatterns = [
 	path('mypage/', views.mypage, name="mypage"),
 	path('product_detail/', views.product_detail, name="product_detail"), #아이디로 바꾸자
 
+
+
+
+	path('mypage/orderhistory', views.orderhistory, name="orderhistory"),
+	path('mypage/ordercancel', views.ordercancel, name="ordercancel"),
+	path('mypage/orderrefund', views.orderrefund, name="orderrefund"),
+
+	path('mypage/couponlist', views.couponlist, name="couponlist"),
+	path('mypage/favoritelist', views.favoritelist, name="favoritelist"),
+	path('mypage/pointlist', views.pointlist, name="pointlist"),
+	path('mypage/qnalist', views.qnalist, name="qnalist"),
+	path('mypage/refundlist', views.refundlist, name="refundlist"),
+	path('mypage/userinfo', views.userinfo, name="userinfo"),
+	path('mypage/reviewlist', views.reviewlist, name="reviewlist"),
+
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def store(request):
     context = {}
@@ -10,13 +11,16 @@ def cart(request):
     context = {}
     return render(request, 'store/cart.html', context)
 
+
 def login(request):
     context = {}
     return render(request, 'store/login.html', context)
 
+
 def register(request):
     context = {}
     return render(request, 'store/register.html', context)
+
 
 def mypage(request):
     context = {}
@@ -27,6 +31,60 @@ def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
 
+
 def product_detail(request):
     context = {}
     return render(request, 'store/productdetail.html', context)
+
+
+
+
+
+
+#주문 관리 view
+def orderhistory(request):
+    context = {}
+    return render(request, 'mypage/orderhistory.html', context)
+
+def ordercancel(request):
+
+    context = {}
+    return render(request, 'mypage/ordercancel.html', context)
+
+def orderrefund(request):
+
+    context = {}
+    return render(request, 'mypage/orderrefund.html', context)
+
+#마이페이지
+def favoritelist(request):
+    context = {}
+    return render(request, 'mypage/favoritelist.html',context)
+
+def couponlist(request):
+    context = {}
+    return render(request, 'mypage/couponlist.html',context)
+
+def orderrefund(request):
+    context = {}
+    return render(request, 'mypage/orderrefund.html',context)
+
+def pointlist(request):
+    context = {}
+    return render(request, 'mypage/pointlist.html',context)
+
+def qnalist(request):
+    context = {}
+    return render(request, 'mypage/qnalist.html',context)
+
+def refundlist(request):
+    context = {}
+    return render(request, 'mypage/refundlist.html',context)
+
+def reviewlist(request):
+    context = {}
+    return render(request, 'mypage/reviewlist.html',context)
+
+def userinfo(request):
+    context = {}
+    return render(request, 'mypage/userinfo.html',context)
