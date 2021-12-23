69ed08d7600c4e036d652d7e22a6de1abc11851e
SWH <sungwoo166@gmail.com>
2021-12-23 18:23:56 +0900

    cart 자바스크립트 추가
    .

---
+++ static/js/cart.js
@@ -0,0 +1,38 @@
+var updateBtns = document.getElementsByClassName('update-cart');
+
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () { //클릭시 function 실행
+        var productId = this.dataset.product;//커스텀 attribute를 통해 데이터를 가져옴 즉, store.html에 있는 product.id가져옴
+        var action = this.dataset.action;// add가져옴 this는 클릭이 된애
+        console.log('productId:', productId, 'Action:', action);
+
+        console.log('USER:', user);
+        if (user == 'AnonymousUser') {
+            console.log('User is not authenticated');
+        } else {
+            updateUserOrder(productId, action);
+        }
+
+
+    });
+}
+
+function updateUserOrder(productId, action) {
+    console.log('User is authenticated')
+
+    var url = '/update_item/'  //POST request 보낼 view url
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({'product': productId, 'action': action})
+    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log('Data:', data)
+        });
+}
