4879a45fd6baf7837e1ec8737b409525aff38be0
SWH <sungwoo166@gmail.com>
2021-12-23 18:26:39 +0900

    CSRF_Token 추가
    .

--- static/js/cart.js
+++ static/js/cart.js
@@ -26,6 +26,7 @@ function updateUserOrder(productId, action) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'X-CSRFToken':csrftoken,
         },
         body: JSON.stringify({'product': productId, 'action': action})
     })
@@ -33,6 +34,6 @@ function updateUserOrder(productId, action) {
             return response.json()
         })
         .then((data) => {
-            console.log('Data:', data)
+            location.reload()
         });
 }
