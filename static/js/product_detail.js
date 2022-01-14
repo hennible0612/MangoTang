b6a41372cd79d5c8a34a752c3a65b1db38cf7aff
SWH <sungwoo166@gmail.com>
2022-01-14 12:03:17 +0900

    product detail javascript 추가
    .

--- static/js/product_detail.js
+++ static/js/product_detail.js
@@ -23,3 +23,20 @@ function morecomment() {
     //style display: none
     // element.style.visibility = show;
 }
+
+var updateBtns = document.getElementsByClassName('add-option');
+for (var i = 0; i < updateBtns.length; i++) {//for문을 돌리는 이유는 모든 updateBtns에게 기능을 추가하기 위함이다.
+    console.log("asdfsadfasfasdf")
+	updateBtns[i].addEventListener('click', function(e){ //클릭시 function 실행
+        e.preventDefault();
+        console.log("option pressed")
+        // var trId = this.dataset.id;
+        // if (document.getElementById(trId).style.visibility == "collapse") {
+        //     document.getElementById(trId).style.visibility = "visible";
+        // } else {
+        //     document.getElementById(trId).style.visibility = "collapse"
+        // }
+
+
+	})
+}
