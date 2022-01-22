8a895dc2280b4aafabaa73f78f3c1e33a72f67cd
SWH <sungwoo166@gmail.com>
2022-01-22 14:33:49 +0900

    리뷰 상세보기 추가
    .

--- static/js/product_detail.js
+++ static/js/product_detail.js
@@ -3,6 +3,7 @@ var updateBtns = document.getElementsByClassName('more-comment');
 for (var i = 0; i < updateBtns.length; i++) {//for문을 돌리는 이유는 모든 updateBtns에게 기능을 추가하기 위함이다.
 	updateBtns[i].addEventListener('click', function(e){ //클릭시 function 실행
         e.preventDefault();
+        console.log("button pressed")
         var trId = this.dataset.id;
         if (document.getElementById(trId).style.visibility == "collapse") {
             document.getElementById(trId).style.visibility = "visible";
