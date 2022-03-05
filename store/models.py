614d5b570659796a6c6b1371d4fc1a65ec0e2bc0
SWH <sungwoo166@gmail.com>
2022-03-05 11:42:29 +0900

    회원가입 버그 수정
    .

--- store/models.py
+++ store/models.py
@@ -10,12 +10,12 @@ class UserForm(UserCreationForm):
     name = forms.CharField(label="이름")
     email = forms.EmailField(label="이메일")
     phone_number = forms.CharField(label="전화번호")
-    address1 = forms.CharField(label="주소")
-    address2 = forms.CharField(label="상세주소")
+    # address1 = forms.CharField(label="주소")
+    # address2 = forms.CharField(label="상세주소")
 
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "name", "email", "phone_number", "address1", "address2")
+        fields = ("username", "password1", "password2", "name", "email", "phone_number")
 
 
 # Create your models here.
@@ -53,12 +53,6 @@ class Product(models.Model):
     shipment_price = models.IntegerField(blank=True)
     hash_tag = models.CharField(max_length=200)
     option_bool = models.BooleanField(default=False, blank=False, null=True)
-    # option_total = models.IntegerField(blank=True, null=True)
-    # option1 = models.ForeignKey("Product", on_delete=models.SET_NULL, null=True, blank=True, related_name="op1")
-    # option2 = models.ForeignKey("Product", on_delete=models.SET_NULL, null=True, blank=True, related_name="op2")
-    # option3 = models.ForeignKey("Product", on_delete=models.SET_NULL, null=True, blank=True, related_name="op3")
-    # option4 = models.ForeignKey("Product", on_delete=models.SET_NULL, null=True, blank=True, related_name="op4")
-    # option5 = models.ForeignKey("Product", on_delete=models.SET_NULL, null=True, blank=True, related_name="op5")
     collection_tag = models.CharField(max_length=200, null=True)
     item_company = models.CharField(null=False, max_length=200)
     product_status = models.BooleanField(default=False, blank=False, null=True)
