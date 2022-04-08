501efbead4281240bc6b1900b057746e22442ca1
SWH <sungwoo166@gmail.com>
2022-04-08 15:29:05 +0900

    모델 주석 추가
    .

--- store/models.py
+++ store/models.py
@@ -33,20 +33,19 @@ class Customer(models.Model):
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=200)  # 제품이름
     price = models.FloatField(null=False, blank=False)
-    image_detail = models.ImageField(null=True, blank=True)
-    image_title = models.ImageField(null=True, blank=True)
-    image_introduce = models.ImageField(null=True, blank=True)
-    seller_code = models.CharField(max_length=50, null=False, blank=False)
-    price_discount = models.FloatField(null=True)
-    discount = models.IntegerField(null=True)
-    stock = models.IntegerField()
-    first_title = models.CharField(max_length=200)
-    second_title = models.CharField(max_length=200)
-    shipment_price = models.IntegerField(blank=True)
-    hash_tag = models.CharField(max_length=200)
-    option_bool = models.BooleanField(default=False, blank=False, null=True)
+    image_detail = models.ImageField(null=True, blank=True)  # 제품 상세 정보
+    image_title = models.ImageField(null=True, blank=True)  # 메인 이미지
+    image_introduce = models.ImageField(null=True, blank=True)  # 제품 소개
+    seller_code = models.CharField(max_length=50, null=False, blank=False)  # 제품
+    price_discount = models.FloatField(null=True)  # 할인된 가격
+    discount = models.IntegerField(null=True)  # 할인율
+    stock = models.IntegerField()  # 수량
+    first_title = models.CharField(max_length=200)  # 제품 상세페이지 메인 타이틀
+    second_title = models.CharField(max_length=200)  # 제품 상세페이지 두 번째 타이틀
+    shipment_price = models.IntegerField(blank=True)  # 배송 비용용    hash_tag = models.CharField(max_length=200)
+    option_bool = models.BooleanField(default=False, blank=False, null=True)  # 옵션의 유무
     collection_tag = models.CharField(max_length=200, null=True)
     item_company = models.CharField(null=False, max_length=200)
     product_status = models.BooleanField(default=False, blank=False, null=True)
@@ -72,21 +71,21 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     order_status = models.BooleanField(default=False)
     payment_state = models.BooleanField(default=False)
-    shipping_fee = models.IntegerField(null=True, blank=True)
+    shipping_fee = models.IntegerField(null=True, blank=True)  # 배송 비용
     order_number = models.IntegerField(null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
-    total_fee = models.IntegerField(null=True, blank=True)
+    total_fee = models.IntegerField(null=True, blank=True)  # 총 가격
     post_code = models.CharField(max_length=200, null=True, blank=True)
     recipent_address1 = models.CharField(max_length=200, null=False)
     recipent_address2 = models.CharField(max_length=200, null=False)
-    recipent_number = models.CharField(max_length=50, null=False)
-    recipent_name = models.CharField(max_length=50, null=False)
-    order_request = models.CharField(max_length=100, null=False)
-    orderer_number = models.CharField(max_length=100, null=False)
-    orderer_name = models.CharField(max_length=100, null=False)
+    recipent_number = models.CharField(max_length=50, null=False)  # 받는 사람 번호
+    recipent_name = models.CharField(max_length=50, null=False)  # 받는 사람 이름
+    order_request = models.CharField(max_length=100, null=False)  # 요청 사항
+    orderer_number = models.CharField(max_length=100, null=False)  # 주문자 번호
+    orderer_name = models.CharField(max_length=100, null=False)  # 주문자 이름
 
     @property
-    def get_all_item_name(self):
+    def get_all_item_name(self):  # 옵션의 이름 다가져오기
         orderitems = self.orderitem_set.all()  # 해당 order의 자식 가져와서
         data = ""
         for name in orderitems:
@@ -95,38 +94,38 @@ class Order(models.Model):
         return data
 
     @property
-    def get_cart_total(self):
+    def get_cart_total(self):  # 카트에 있는 총 비용 (옵션 제외)
         orderitems = self.orderitem_set.all()  # 해당 order의 자식 가져와서
         total = sum([item.get_total for item in orderitems])  # 다더함함
         return total
 
     @property
-    def get_cart_items(self):
+    def get_cart_items(self):  # 카트에 있는 총 수량 (옵션 제외)
         orderitems = self.orderitem_set.all()
         total = sum([item.quantity for item in orderitems])
         return total
 
     @property
-    def get_cart_option_total(self):
+    def get_cart_option_total(self):  # 카트의 있는 옵션의 총 비용
         orderitems = self.orderitem_set.all()  # 해당 order의 자식 가져와서
         total = sum([item.get_option_cart_total for item in orderitems])  # 다더함함
         return total
 
     @property
-    def get_total(self):  # 옵션 포함 해당 order의 모든 가격
+    def get_total(self):  # 카트에 있는 옵션, 제품의 가격
         orderitems = self.orderitem_set.all()  # 해당 order의 자식 가져와서
         total = sum([item.get_total for item in orderitems])  # 다더함함
         optionTotal = sum([item.get_option_cart_total for item in orderitems])  # 다더함함
         return total + optionTotal
 
     @property
-    def get_deliver_price(self):
+    def get_deliver_price(self):  # 카트에 있는 모든 옵션의 가격
         orderitems = self.orderitem_set.all()
         total = sum([item.get_delivery_price for item in orderitems])
         return total
 
     @property
-    def get_all_price(self):
+    def get_all_price(self):  # 카트에 있는 옵션, 제품, 배송비 포함 가격
         orderitems = self.orderitem_set.all()
         deliverTotal = sum([item.get_delivery_price for item in orderitems])
         itemTotal = sum([item.get_total for item in orderitems])  # 다더함함
@@ -146,13 +145,10 @@ class OrderHistory(models.Model):
     order_name = models.CharField(max_length=200, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     date_completed = models.DateTimeField(null=True, blank=True)
-    track_number = models.IntegerField(null=True, blank=True)
-
-    # deliver_state = models.CharField(max_length=200, null=True, blank=True)
-
-    payment_state = models.BooleanField(default=False)
+    track_number = models.IntegerField(null=True, blank=True)  # 송장번호
+    payment_state = models.BooleanField(default=False)  # 결제 상태
     shipping_fee = models.IntegerField(null=True, blank=True)
-    order_number = models.IntegerField(null=True, blank=True)
+    order_number = models.IntegerField(null=True, blank=True)  # 주문번호
     email = models.EmailField(null=True, blank=True)
     total_fee = models.IntegerField(null=True, blank=True)
     post_code = models.CharField(max_length=200, null=True, blank=True)
@@ -163,12 +159,12 @@ class OrderHistory(models.Model):
     order_request = models.CharField(max_length=100, null=False)
     orderer_number = models.CharField(max_length=100, null=False)
     orderer_name = models.CharField(max_length=100, null=False)
-
-    receipt_url = models.CharField(max_length=500, null=True, blank=True)
-    status = models.CharField(max_length=100, null=True, blank=True)
-    emb_pg_provider = models.CharField(max_length=100, null=True, blank=True)
-    imp_uid = models.CharField(max_length=100, null=True, blank=True)
-    pay_method = models.CharField(max_length=100, null=True, blank=True)
+    receipt_url = models.CharField(max_length=500, null=True, blank=True)  # 아임포트 결제 영수증
+    status = models.CharField(max_length=100, null=True, blank=True)  # 주문 상태
+    emb_pg_provider = models.CharField(max_length=100, null=True,
+                                       blank=True)  # 아임포트 결제 방법 (네이버페이, 카카오 페에, 토스 페이... etc)
+    imp_uid = models.CharField(max_length=100, null=True, blank=True)  # 아임포트 주문번호
+    pay_method = models.CharField(max_length=100, null=True, blank=True)  # 결제방법 (카드, 무통장... etc)
 
     @property
     def get_total(self):
@@ -198,22 +194,19 @@ class OrderItem(models.Model):
         ('46', 'CU편의점택배'),
         ('24', 'GS Postbox 택배	'),
     )
-
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     orderHistory = models.ForeignKey(OrderHistory, on_delete=models.SET_NULL, null=True)
     deliver_state = models.CharField(max_length=200, null=True, blank=True)
-
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
     item_option_bool = models.BooleanField(default=False, blank=False, null=True)
-
     deliver_company = models.CharField(max_length=200, choices=DELIVER_COMPANY, null=True, blank=True)
-    track_number = models.IntegerField(null=True, blank=True)
+    track_number = models.IntegerField(null=True, blank=True) # 송장번호
 
     @property
-    def get_all_option_name(self):
-        orderOptions = self.orderitemoption_set.all()  # 해당 order의 자식 가져와서
+    def get_all_option_name(self): # orderitemoption의 이름을 가지고 온다.
+        orderOptions = self.orderitemoption_set.all()
         data = ""
         for name in orderOptions:
             data += name.get_option_name + ", "
@@ -237,10 +230,10 @@ class OrderItem(models.Model):
         total = self.product.price_discount * self.quantity
         return total
 
-    @property  # 옵션 가격
+    @property  # 모든 옵션 가격
     def get_option_cart_total(self):
-        orderOptions = self.orderitemoption_set.all()  # 해당 order의 자식 가져와서
-        total = sum([item.get_total for item in orderOptions])  # 다더함함
+        orderOptions = self.orderitemoption_set.all()
+        total = sum([item.get_total for item in orderOptions])
         return total
 
     @property  # 옵션 포함 가격
@@ -274,7 +267,7 @@ class OrderItemOption(models.Model):
             self.get_total)
 
 
-class ShippingAddress(models.Model):
+class ShippingAddress(models.Model): # 구현해야할 부분 여러가지 배송 정보를 저장하고 싶을때 사용
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=200, null=False)
@@ -299,10 +292,9 @@ class ProductReview(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
     star_rating = models.IntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)], blank=True,
                                       null=True)
-    order_number = models.IntegerField(null=True, blank=True)
-
-    short_review = models.CharField(max_length=50, null=True, blank=True)
-    long_review = models.CharField(max_length=200, null=True, blank=True)
+    order_number = models.IntegerField(null=True, blank=True)  # 주문번호
+    short_review = models.CharField(max_length=50, null=True, blank=True)  # 한줄 리뷰
+    long_review = models.CharField(max_length=200, null=True, blank=True)  # 상세 리뷰
     date_added = models.DateTimeField(default=datetime.now)
     image = models.ImageField(null=True, blank=True)
     review_bool = models.BooleanField(default=False, blank=False, null=True)
